2b34fa2cbcd2ef29af7f157776b8c98655f0d278
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-06 14:39:04 +0100

    import deps

---
+++ model1.py
@@ -0,0 +1,5 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+import tensorflow as tf
