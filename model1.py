d074d2338fe160410f0ed1447d62060319176ee2
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-06 14:39:31 +0100

    load tweets data

--- model1.py
+++ model1.py
@@ -3,3 +3,10 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental.preprocessing import StringLookup
 import tensorflow as tf
+
+with open("./tweets/lfact.txt") as f:
+    _data = f.read().splitlines()
+    data = list(set(_data))
+
+_train = []
+train = []
